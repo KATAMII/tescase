1c16586f4b195ab3b568209f30d1d3a1d66f0f6e
KATAMII <lydiamwaah01@gmail.com>
2024-05-27 16:31:03 +0300

    refactor the code

--- main.js
+++ main.js
@@ -1,12 +1,9 @@
 module.exports ={fizzbuzz};
 function fizzbuzz(number){
-    if (number % 3 === 0 && number % 5 ===0) {
-        return "fizzbuzz";
-    } else if (number % 3 === 0) {
-        return "fizz";
-    } else if (number % 5 === 0) {
-        return "buzz";
-    } return number;
+    if (number % 3 === 0 && number % 5 ===0) return "fizzbuzz";
+    if (number % 3 === 0) return "fizz";
+    if (number % 5 === 0) return "buzz";
+    return number;
 }
 console.log (fizzbuzz(45));
 console.log (fizzbuzz(1000));
